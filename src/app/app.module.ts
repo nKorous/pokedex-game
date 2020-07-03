9083432158952d299555cc3a7d100873133cea52
Nate Korous <development.nk@gmail.com>
2020-07-03 10:58:38 -0600

    main page setup, starting game select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button/'
+import { MatCardModule } from '@angular/material/card';
+import { GameSelectComponent } from './game-select/game-select.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameSelectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
