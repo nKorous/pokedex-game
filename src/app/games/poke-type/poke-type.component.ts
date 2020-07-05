0b5a9d76602c1a24cba0ac2e214681ab265ecf88
Nate Korous <development.nk@gmail.com>
2020-07-05 17:58:02 -0600

    finished pokeType

--- src/app/games/poke-type/poke-type.component.ts
+++ src/app/games/poke-type/poke-type.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerService } from '../../services/player.service'
+import { DataService } from '../../services/data.service'
+import { Pokemon } from '../../models/pokemon';
 
 @Component({
   selector: 'app-poke-type',
@@ -6,10 +9,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poke-type.component.css']
 })
 export class PokeTypeComponent implements OnInit {
+  pokemon: Pokemon;
+  alreadyGuessed: Array<number> = new Array();
+  lastPokemon: { name: string, type1: string, type2?: string, correct: boolean };
+  pokeTypes: Array<string> = new Array()
 
-  constructor() { }
+  constructor(
+    private playerService: PlayerService,
+    private dataService: DataService
+  ) {}
 
   ngOnInit(): void {
+    this.getNextPokemon()
+    this.getPokeTypes()
   }
 
+  getNextPokemon() {
+    let pokemon = this.dataService.getRandomPokemon();
+    if (this.alreadyGuessed.find(f => f === pokemon.pokedexNumber)) {
+      this.getNextPokemon();
+    } else {
+      this.pokemon = pokemon;
+      this.alreadyGuessed = this.alreadyGuessed.length > 250 ? [...this.alreadyGuessed, pokemon.pokedexNumber] : [ pokemon.pokedexNumber ]
+    }
+  }
+
+  getTypeColor(type: string) {
+    return `var(--${type}-color)`
+  }
+
+  checkAnswer(guess: string) {
+    if(guess === this.pokemon.type1 || guess === this.pokemon.type2) {
+      this.playerService.addPlayerPoints(5)
+      this.lastPokemon = { name: this.pokemon.name, type1: this.pokemon.type1, type2: this.pokemon.type2, correct: true}
+      this.getNextPokemon()
+    } else {
+      this.lastPokemon = { name: this.pokemon.name, type1: this.pokemon.type1, type2: this.pokemon.type2, correct: false}
+      this.getNextPokemon()
+    }
+  }
+
+  getPokeTypes() {
+    this.dataService.getTypes().subscribe(types => {
+      this.pokeTypes = types.sort((a, b) => {
+        return a < b ? 1 : b < a ? -1 : 0
+      })
+    })
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,36 +1,60 @@
 import { Injectable } from '@angular/core';
 import { Pokemon } from '../models/pokemon';
-import { PokemonList } from '../data/pokemon'
+import { PokemonList } from '../data/pokemon';
 import { of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-  private pokeList: Array<Pokemon> = PokemonList
+  private pokeList: Array<Pokemon> = PokemonList;
+  private pokeTypes: Array<string> = [
+    'bug',
+    'dark',
+    'dragon',
+    'electric',
+    'fairy',
+    'fighting',
+    'fire',
+    'flying',
+    'ghost',
+    'grass',
+    'ground',
+    'ice',
+    'normal',
+    'poison',
+    'psychic',
+    'rock',
+    'steel',
+    'water',
+  ];
 
-  constructor() { 
-  }
+  constructor() {}
 
   getPokeList() {
-    return of(this.pokeList)
+    return of(this.pokeList);
   }
 
   getPokemonGeneration(generation: number) {
-    return of(this.pokeList.filter(p => p.generation === generation))
+    return of(this.pokeList.filter((p) => p.generation === generation));
   }
 
   getPokemonType(type: string) {
-    return of(this.pokeList.filter(p => p.type1 === type || p.type2 === type))
+    return of(
+      this.pokeList.filter((p) => p.type1 === type || p.type2 === type)
+    );
   }
 
-  getLegendaryOnly(){
-    return of(this.pokeList.filter(p => p.is_legendary))
+  getLegendaryOnly() {
+    return of(this.pokeList.filter((p) => p.is_legendary));
   }
 
   getRandomPokemon() {
-    let rand = Math.floor((Math.random() * this.pokeList.length) + 1)
-    return this.pokeList[rand]
+    let rand = Math.floor(Math.random() * this.pokeList.length + 1);
+    return this.pokeList[rand];
+  }
+
+  getTypes() {
+    return of(this.pokeTypes)
   }
 }
-
