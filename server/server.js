6c1e21945f9b8514a27997e4f4680d8f9c7d557a
Nate Korous <development.nk@gmail.com>
2020-10-16 08:20:39 -0600

    added server.js to server on heroku and updated package.json

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.listen(process.env.PORT || 8080);
+
+const staticDist = "./dist/pokedex-game";
+
+app.use(express.static(staticDist));
+
+// root
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: staticDist });
+});
