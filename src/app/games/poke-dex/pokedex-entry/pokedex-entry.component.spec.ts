429d7dd14561e5c0d10f75eaa72c0e4a3b5b017a
Nate Korous <nathaniel.korous@gmail.com>
2023-05-02 13:04:22 -0600

    update cli to angular 11

--- src/app/games/poke-dex/pokedex-entry/pokedex-entry.component.spec.ts
+++ src/app/games/poke-dex/pokedex-entry/pokedex-entry.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PokedexEntryComponent } from './pokedex-entry.component';
 
@@ -6,7 +6,7 @@ describe('PokedexEntryComponent', () => {
   let component: PokedexEntryComponent;
   let fixture: ComponentFixture<PokedexEntryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PokedexEntryComponent ]
     })
